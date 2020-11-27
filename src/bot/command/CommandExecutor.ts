9590c41e5a1b5673abfd1a4edfef2347b50ebf1a
Kim Kyu San <developerstarlight@gmail.com>
2020-11-27 20:54:15 +0900

    CommandInfo interface

--- src/bot/command/CommandExecutor.ts
+++ src/bot/command/CommandExecutor.ts
@@ -2,9 +2,25 @@ import Logger from "../../Logger";
 import Asteroid from "../Asteroid";
 import { Message } from "discord.js";
 
+export interface CommandInfo {
+  name: string,
+  alias: string[],
+  desc: string,
+  props: number,
+  isAdminOnly: boolean
+}
+
 class CommandExecutor {
   public execute(client: Asteroid, msg: Message, args?: string[]): void {
     Logger.err('Execute command is not defined')
+  }
+
+  public info: CommandInfo = {
+    name: 'not-defined',
+    alias: [],
+    desc: 'Warning! this command is not defined correctly',
+    props: -1,
+    isAdminOnly: false
   }
 }
 
