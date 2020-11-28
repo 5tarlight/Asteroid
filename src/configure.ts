b24276f9c16f4e4d83aa9889ccedeb079c6e5c92
Kim Kyu San <developerstarlight@gmail.com>
2020-11-28 12:13:40 +0900

    database config

--- src/configure.ts
+++ src/configure.ts
@@ -2,17 +2,28 @@ import InvalidEnvError from "./error/InvalidEnvError";
 
 export const checkEnv = () => {
   const configs = Object.values(config())
+  const check = (cfg) => {
+    cfg.forEach(c => {
+      if (!c) throw new InvalidEnvError(".env file is not valid. Check src/configure.ts")
+      if (typeof c === 'object' && c !== null) check(Object.values(c))
+    })
+  }
 
-  configs.forEach(c => {
-    if (!c) throw new InvalidEnvError(".env file is not valid. Check src/configure.ts")
-  })
+  check(configs)
 }
 
 const config = () => ({
   botToken: process.env.BOT_TOKEN,
   prefix: process.env.PREFIX,
   primaryServer: process.env.PRIMARY_SERVER_ID,
-  primaryChannel: process.env.PRIMARY_CHANNEL_ID
+  primaryChannel: process.env.PRIMARY_CHANNEL_ID,
+  db: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  }
 })
 
 export default config
