8dfc8fc8679e2cd9d82880c6876f18b80cfdfadf
5tarlight <developerstarlight@gmail.com>
2020-12-04 17:03:10 +0900

    planet info command desc

---
+++ src/bot/command/PlanetInfo.ts
@@ -0,0 +1,19 @@
+import CommandExecutor, { CommandInfo } from "./CommandExecutor";
+import Asteroid from "../Asteroid";
+import { Message } from "discord.js";
+
+class PlanetInfo implements CommandExecutor {
+  info: CommandInfo = {
+    name: 'planetinfo',
+    desc: '행성의 정보를 보여줍니다.',
+    alias: ['행성', 'pi', '플라넷', '행성정보'],
+    props: 1,
+    isAdminOnly: false
+  }
+
+  execute(client: Asteroid, msg: Message, args: string[]): void {
+  }
+
+}
+
+export default PlanetInfo
