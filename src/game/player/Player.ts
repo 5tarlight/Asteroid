e2834bd21ef6bc0e5a573603435ea3e5bd982049
5tarlight <developerstarlight@gmail.com>
2020-12-09 23:21:35 +0900

    player superclass

--- src/game/player/Player.ts
+++ src/game/player/Player.ts
@@ -1,5 +1,19 @@
-class Player {
+import Inventory from "./Inventory";
 
+class Player {
+  username: string
+  discord: string
+  exp: number
+  money: number
+  inv: Inventory
+
+  constructor(username: string, discord: string) {
+    this.username = username
+    this.discord = discord
+    this.money = 1000
+    this.exp = 0
+    this.inv = new Inventory()
+  }
 }
 
 export default Player
