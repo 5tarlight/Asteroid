f84dcfd6be72409fdf19ab527f64b54999463bb4
5tarlight <developerstarlight@gmail.com>
2020-12-10 13:04:06 +0900

    self check user info

--- src/bot/command/PlayerInfo.ts
+++ src/bot/command/PlayerInfo.ts
@@ -1,6 +1,6 @@
 import CommandExecutor, { CommandInfo } from "./CommandExecutor";
 import Asteroid from "../Asteroid";
-import { Message, MessageEmbed } from "discord.js";
+import { Message, MessageEmbed, User } from "discord.js";
 import { Users } from "../../util/Database";
 import { Op } from "sequelize";
 
@@ -13,40 +13,45 @@ class PlayerInfo implements CommandExecutor {
     props: 1
   }
 
-  async execute(client: Asteroid, msg: Message, args: string[]) {
-    const mention = msg.mentions.users.first()
-    if (mention) {
-      const user = await Users.findAll({
-        where: {
-          discord: { [Op.eq]: mention.id }
-        }
-      })
-
-      if (user.length < 1) {
-        const embed = new MessageEmbed()
-          .setTitle('Error 404: NotFound')
-          .setDescription(`유저 ${mention.tag}를 찾을 수 없습니다.`)
-
-        msg.channel.send(embed)
-        return
+  async findUser(mention: User, msg: Message) {
+    const user = await Users.findAll({
+      where: {
+        discord: { [Op.eq]: mention.id }
       }
+    })
 
-      // @ts-ignore
-      const created = user[0].get('createdAt').toString().split(' ').slice(0, 5).join(' ')
-      // @ts-ignore
-      const updated = user[0].get('updatedAt').toString().split(' ').slice(0, 5).join(' ')
-
+    if (user.length < 1) {
       const embed = new MessageEmbed()
-        .setTitle(mention.tag)
-        .addField('id', user[0].get('id'), true)
-        .addField('경험치', user[0].get('exp'), true)
-        .addField('가입일', created, true)
-        .addField('마지막 갱신', updated, true)
+        .setTitle('Error 404: NotFound')
+        .setDescription(`유저 ${mention.tag}를 찾을 수 없습니다.`)
 
       msg.channel.send(embed)
       return
     }
-    else msg.channel.send('no mention detected')
+
+    // @ts-ignore
+    const created = user[0].get('createdAt').toString().split(' ').slice(0, 5).join(' ')
+    // @ts-ignore
+    const updated = user[0].get('updatedAt').toString().split(' ').slice(0, 5).join(' ')
+
+    const embed = new MessageEmbed()
+      .setTitle(mention.tag)
+      .addField('id', user[0].get('id'), true)
+      .addField('경험치', user[0].get('exp'), true)
+      .addField('가입일', created, true)
+      .addField('마지막 갱신', updated, true)
+
+    msg.channel.send(embed)
+  }
+
+  execute(client: Asteroid, msg: Message, args: string[]) {
+    const mention = msg.mentions.users.first()
+    if (mention) {
+      this.findUser(mention, msg)
+    } else {
+      if (args.length < 1)
+        this.findUser(msg.author, msg)
+    }
   }
 }
 
